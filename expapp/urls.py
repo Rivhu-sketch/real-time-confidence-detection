629b59b3850c1b0205f1bbf6200f7c17757b48e2
Rivhu-sketch <rivhu.shil.formularvs@gmail.com>
2025-04-13 21:56:45 +0530

    Add files via upload

---
+++ expapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('home/',views.home,name='home'),
+    path('start/', views.start_detection, name='start_detection'),
+
+]
---
+++ views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def expapp(request):
+    return render(request,)
